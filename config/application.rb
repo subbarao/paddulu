d04875fd789fe0afb9f325a09663755471bfa273
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2014-07-13 19:00:55 -0400

    something

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module RailsBootstrap
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
