9928e76ad9646aacd8d6f5ee902b16b52f495413
battula <prabhakar.battula@gmail.com>
2014-07-16 22:49:38 +0530

    added validations to bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,6 @@
 class Bill < ActiveRecord::Base
   belongs_to :user
+  validates :store, presence: true
+	#validates_numericality_of :total, only_float: true, greater_than: 0
+  validates :total, :format => {:with => /\A\d+(?:\.\d{0,2})?\z/, message: "should be in dollars and cents and seperated by dot"}, :numericality => {:greater_than => 0}
 end
